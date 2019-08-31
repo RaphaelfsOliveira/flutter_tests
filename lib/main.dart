5868b2988e5a0de8f6834dbc58f54bd47d5d6e15
Raphael Oliveira <raphaelbrf@gmail.com>
2019-08-31 20:48:33 -0300

    trocando o tema

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: RandomWords(),      
     );
   }
 }
